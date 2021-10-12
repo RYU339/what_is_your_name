84ac9191cec85820f5b777a5373b1b5060e7f30c
RYU <ryuryuryu339@likelion.org>
2021-10-12 17:25:54 +0900

    TEAM Coprime #1

---
+++ can/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from can import views
+
+app_name = 'can'
+
+urlpatterns = [
+    path('img_upload/', views.img_upload, name='img_upload'),
+    path('predict/', views.pred_img, name='pred_img'),
+    path('detect_text/<str:file_name>/', views.detect_text, name='detect_text'),
+    path('delete/<str:file_name>', views.delete, name='delete')
+]
